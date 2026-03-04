744fd6d04f9bd18bc6abe392c26233b4d8192701
MaelleEschTeko <maelle.esch@edu.teko.ch>
2026-03-04 20:25:43 +0100

    funktion fertig

--- Code/Function_CheckNewImport.py
+++ Code/Function_CheckNewImport.py
@@ -1,2 +1,16 @@
-def CheckNewImport():
+import json
+from pathlib import Path
+from typing import List
+
+
+def function_CheckNewImport(RemainingQuestions):
+    # Nur importieren, wenn keine Fragen mehr übrig sind
+    if RemainingQuestions:
+        return False
     
+
+    # Block 1 entfernen und neuer Block wird zu Block 0
+    if not RemainingQuestions:
+        QuestionJSON = RemainingQuestions[:0]
+        RemainingQuestions.pop(0)
+        return QuestionJSON, RemainingQuestions
