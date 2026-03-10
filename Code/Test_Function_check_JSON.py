1074b6845e6b1723f44f7fa65bb8d45922d42ec3
DavidAndristTeko <david.andrist@edu.teko.ch>
2026-03-10 19:02:34 +0100

    Tests geschrieben

---
+++ Code/Test_Function_check_JSON.py
@@ -0,0 +1,13 @@
+import pytest 
+from Function_check_JSON import function_check_answer
+
+yes = "y"
+no = "no"
+Option_1 = {'text': 'Elefant', 'correct': 'y'}
+Option_2 = {'text': 'Elefant', 'correct': 'n'}
+
+def test_function_check_answer():
+    assert function_check_answer(yes, Option_1) == True
+    assert function_check_answer(yes, Option_2) == False
+    assert function_check_answer(no, Option_1) == False
+    assert function_check_answer(no, Option_2) == True
