bbff0d409e3054dbe0c8653d62c4d70fd6fc8caa
DavidAndristTeko <david.andrist@edu.teko.ch>
2026-03-17 17:59:07 +0100

    Unnötigen Code entfernt

--- Code/Function_CheckNewImport.py
+++ Code/Function_CheckNewImport.py
@@ -4,9 +4,6 @@ def function_CheckNewImport(RemainingQuestions):
     
     :param RemainingQuestions: Enthält alle übrigen Frageblöcke aus dem JSON, die der Spieler noch nicht beantwortet hat 
     """
-    # wenn keine Fragen mehr übrig sind
-    if not RemainingQuestions:
-        return [], RemainingQuestions
 
     # nächste Frage holen
     QuestionJSON = [RemainingQuestions.pop(0)]
